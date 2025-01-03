13ec7342de8a47bb322d0e277cf042d04f73996c
Caetano Genete <caetanogenete@gmail.com>
2025-01-03 13:42:12 +0000

    Fixed vim environment not working with LSP

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -39,28 +39,28 @@ return {
 			)
 
 			-- LSP setup here
-
 			lspconfig.lua_ls.setup({
-				on_init = function(client)
-					if client.workspace_folders then
-						local path = client.workspace_folders[1].name
-						if vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc") then
-							return
-						end
-					end
-
-					client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+				settings = {
+					Lua = {
 						runtime = {
+							-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
 							version = "LuaJIT",
+							path = vim.split(package.path, ";"),
+						},
+						diagnostics = {
+							-- Get the language server to recognize the `vim` global
+							globals = { "vim" },
 						},
 						workspace = {
+							-- Make the server aware of Neovim runtime files and plugins
+							library = { vim.env.VIMRUNTIME },
 							checkThirdParty = false,
-							library = {
-								vim.env.VIMRUNTIME,
-							},
 						},
-					})
-				end,
+						telemetry = {
+							enable = false,
+						},
+					},
+				},
 			})
 			lspconfig.clangd.setup({})
 			lspconfig.cmake.setup({})
