67514e8a03b0c4bcc4da7134c99095387588b6e5
Caetano Genete <caetanogenete@gmail.com>
2025-01-03 00:12:30 +0000

    added nvim runtime to lua_ls

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -12,9 +12,7 @@ return {
 		"neovim/nvim-lspconfig",
 		config = function()
 			-- Add rounded border to hover window
-			vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-				border = "rounded",
-			})
+			vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
 		end,
 	},
 	{
@@ -41,7 +39,29 @@ return {
 			)
 
 			-- LSP setup here
-			lspconfig.lua_ls.setup({})
+
+			lspconfig.lua_ls.setup({
+				on_init = function(client)
+					if client.workspace_folders then
+						local path = client.workspace_folders[1].name
+						if vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc") then
+							return
+						end
+					end
+
+					client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+						runtime = {
+							version = "LuaJIT",
+						},
+						workspace = {
+							checkThirdParty = false,
+							library = {
+								vim.env.VIMRUNTIME,
+							},
+						},
+					})
+				end,
+			})
 			lspconfig.clangd.setup({})
 			lspconfig.cmake.setup({})
 			lspconfig.gopls.setup({
