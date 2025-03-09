9d0acdeef28938cb398e642aaf4cd535f834809c
Caetano Genete <caetanogenete@gmail.com>
2025-03-09 21:35:49 +0000

    minor fixes

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,3 +1,14 @@
+vim.api.nvim_create_user_command("AutoFmtToggle", function(args)
+	if args.bang then
+		vim.b.disable_autoformat = not (vim.b.disable_autoformat or false)
+	else
+		vim.g.disable_autoformat = not (vim.g.disable_autoformat or false)
+	end
+end, {
+	desc = "Toggle autoformat-on-save. If suffixed with a bang (!), will toggle only for the current buffer.",
+	bang = true,
+})
+
 ---@module "lazy"
 ---@type LazyPluginSpec
 return {
@@ -18,9 +29,15 @@ return {
 	---@type conform.setupOpts
 	opts = {
 		formatters_by_ft = require("config.user-defaults.config").formatters_by_ft,
-		format_on_save = {
-			timeout_ms = 500,
-			lsp_format = "fallback",
-		},
+		format_on_save = function(bufnr)
+			if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+				return
+			end
+
+			return {
+				timeout_ms = 500,
+				lsp_format = "fallback",
+			}
+		end,
 	},
 }
